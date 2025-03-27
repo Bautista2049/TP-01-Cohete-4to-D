1b8c27e8557c50492d04bbb4d768744968b3e24e
Bautista2049 <49379221@est.ort.edu.ar>
2025-03-27 10:33:13 -0300

    TP entero menos fuego

--- Assets/Scripts/BotonScript.cs
+++ Assets/Scripts/BotonScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BotonScript : MonoBehaviour
 {
     public GameObject botonGO;
+    public CoheteScript CoheteScript;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class BotonScript : MonoBehaviour
     void OnMouseDown()
     {
         botonGO.SetActive(false);
+        CoheteScript.Despegue();
     }
 
     void OnMouseUp()
